5e1bd1c6cb1cf09045aa01b9f312016bc0eca211
Camposm97 <camposm1205@gmail.com>
2020-05-24 23:32:20 -0400

    Updated App
    Displays menu bar for this project.

--- src/app/App.java
+++ src/app/App.java
@@ -1,19 +1,15 @@
 package app;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import view.MiStage;
 
 public class App extends Application {
-	static String TITLE = "Pixel Inverter";
+	public static String TITLE = "Pixel Inverter";
 	
 	@Override
 	public void start(Stage stage) throws Exception {
-		ImageView iv = new ImageView("cat.jpg");
-		stage.setScene(new Scene(new StackPane(iv)));
-		stage.setTitle(TITLE);
+		stage = new MiStage();
 		stage.show();
 	}
 	
