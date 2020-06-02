1b5cb955d5d518a74a7af11a6ade0c3d5d3d5d28
Camposm97 <camposm1205@gmail.com>
2020-06-01 21:48:23 -0400

    Working on MenuBar
    Connecting Kiefers code to JavaFX

--- src/view/MiMenuBar.java
+++ src/view/MiMenuBar.java
@@ -1,40 +1,49 @@
 package view;
 
+import javafx.application.Platform;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 
 /**
  * Loads the menus: File, Edit, Window, and Help
+ * 
  * @author Camposm97
  */
 public class MiMenuBar extends MenuBar {
 	private Menu mFile, mEdit, mWindow, mHelp;
-	private MenuItem miLoad, miSaveAs, miExit;
+	private MenuItem miOpen, miSaveAs, miExit;
 	private MenuItem miColorNeg;
 	private MenuItem miAbout;
-	
+
 	public MiMenuBar() {
-		// Init Menu Items
-		miLoad = new MenuItem("Load");
+		initMenuItems();
+		addMenuItems();
+	}
+
+	public void initMenuItems() {
+		miOpen = new MenuItem("Open");
 		miSaveAs = new MenuItem("Save As");
 		miExit = new MenuItem("Exit");
+		miExit.setOnAction(event -> {
+			Platform.exit();
+		});
 		miColorNeg = new MenuItem("Color Negative");
+		miColorNeg.setOnAction(event -> {
+			
+		});
 		miAbout = new MenuItem("About");
-		
+	}
+
+	public void addMenuItems() {
 		// Init Menus and Add Menu Items
 		mFile = new Menu("File");
-		mFile.getItems().addAll(miLoad, miSaveAs, miExit);
-		
+		mFile.getItems().addAll(miOpen, miSaveAs, miExit);
 		mEdit = new Menu("Edit");
 		mEdit.getItems().addAll(miColorNeg);
-		
 		mWindow = new Menu("Window");
-		
-		
 		mHelp = new Menu("Help");
 		mHelp.getItems().addAll(miAbout);
-		
 		// Add Menus
 		super.getMenus().addAll(mFile, mEdit, mWindow, mHelp);
 	}
